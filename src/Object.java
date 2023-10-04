ebc7238dced63be57b512fff6e70f26096a20b16
Zhao Zhang <zhaoz@nycstudents.net>
2023-10-04 09:06:56 -0400

    commit

--- src/Object.java
+++ src/Object.java
@@ -1,2 +1,47 @@
-package PACKAGE_NAME;public class Object {
+public class Object {
+    private String color;
+    private int numObj;
+    private double costEach;
+    public Object(String color, int numObj, double cost){
+        this.color = color;
+        this.numObj = numObj;
+        this.costEach = cost;
+    }
+    public Object(){
+        color = "red";
+        numObj = 2;
+        costEach = 4.72;
+    }
+    public String getColorObj(){
+        return color;
+    }
+    public int getNumObj(){
+        return numObj;
+    }
+    public double getCost(){
+        return costEach;
+    }
+    public void setColor(String color){
+        this.color = color;
+    }
+    public void setNumObj(int numObj){
+        this.numObj = numObj;
+    }
+    public void setCost(double cost){
+        this.costEach = cost;
+    }
+    public void introduceCost(){
+        System.out.println("1 "+color+" bandana will cost you $"+numObj);
+    }
+    public void purchaseBandanas(int numBuy){
+        if (numBuy>numObj){
+            System.out.println("Sorry, we don't have "+numBuy+" bandanas, we only have "+numObj+" bandanas in stock currently.");
+        } else {
+            System.out.println(numBuy+" "+color+" bandanas will cost you $"+numBuy*costEach);
+        }
+    }
+    public void useDiscount(double discountDecimals){
+        this.costEach = costEach*(1-discountDecimals);
+        System.out.println("The discount has been applied.");
+    }
 }
--- src/ObjectRunner.java
+++ src/ObjectRunner.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class ObjectRunner {
+public class ObjectRunner {
+    public static void main(String[] args){
+        Object obj1 = new Object("blue",4,2.5);
+        obj1.introduceCost();
+        obj1.purchaseBandanas(5);
+        obj1.useDiscount(0.2);
+        obj1.purchaseBandanas(4);
+        Object obj2 = new Object();
+        System.out.println("The color of this bandana is "+obj2.getColorObj());
+        System.out.println("There are "+obj2.getNumObj()+" bandanas.");
+        System.out.println("The cost of this bandana is "+obj2.getCost());
+    }
 }
